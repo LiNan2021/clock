ea27695cdbf3cd3f6f27c75e13b91c1df1e2e5a6
LiNan2021 <91060590+LiNan2021@users.noreply.github.com>
2023-06-27 08:17:11 +0800

    Create focus-clock.py

---
+++ focus-clock.py
@@ -0,0 +1,15 @@
+import time
+
+def countdown(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
+
+focus_time = 25  # 设置专注时间，单位为分钟
+
+countdown(focus_time)
